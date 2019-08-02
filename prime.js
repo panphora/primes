cfb66fd0a1c3af8a01b1f061394a935afb412c14
David Miranda <david@storylog.com>
2019-08-02 17:31:20 -0400

    update

--- prime.js
+++ prime.js
@@ -1,5 +1,42 @@
+const fs = require('fs');
 const lineReader = require('line-reader');
 
-lineReader.eachLine('./numbers.txt', function(line) {
-  console.log(parseInt(line, 10));
+function isPrime(num) {
+  for(var i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+
+  return num > 1;
+}
+
+function findNextPrime (num) {
+  num = num + 1;
+
+  if (isPrime(num)) {
+    return num;
+  } else {
+    return findNextPrime(num);
+  }
+}
+
+
+lineReader.eachLine('./number.txt', function(line) {
+  let num = parseInt(line, 10);
+
+  if (isNaN(num)) {
+    throw new Error("Not a number!");
+  }
+
+  let nextPrime = findNextPrime(num);
+
+  fs.writeFileSync("./number.txt", nextPrime)
 });
+
+
+
+
+
+
+
